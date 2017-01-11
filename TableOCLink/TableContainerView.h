75f6aef207ff8946840e38773db781330a26aef5
wuyt <594228924@qq.com>
2017-01-11 23:45:35 +0800

    add simple Table

---
+++ TableOCLink/TableContainerView.h
@@ -0,0 +1,13 @@
+//
+//  TableContainerView.h
+//  TableOCLink
+//
+//  Created by 吴玉铁 on 2017/1/11.
+//  Copyright © 2017年 wuyutie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableContainerView : UIView
+
+@end
---
+++ TableOCLink/TableContainerView.m
@@ -0,0 +1,21 @@
+//
+//  TableContainerView.m
+//  TableOCLink
+//
+//  Created by 吴玉铁 on 2017/1/11.
+//  Copyright © 2017年 wuyutie. All rights reserved.
+//
+
+#import "TableContainerView.h"
+
+@implementation TableContainerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
